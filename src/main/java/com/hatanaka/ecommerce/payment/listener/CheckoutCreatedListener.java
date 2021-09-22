5621adfbf46830c3e4d2bae6be8b07c5c0ccc0d2
schott-anderson <schott.anderson@hotmail.com>
2021-09-22 14:37:27 -0300

    Atualiza API para receber dados da API de checkout

--- src/main/java/com/hatanaka/ecommerce/payment/listener/CheckoutCreatedListener.java
+++ src/main/java/com/hatanaka/ecommerce/payment/listener/CheckoutCreatedListener.java
@@ -1,14 +1,29 @@
 package com.hatanaka.ecommerce.payment.listener;
 
+import com.hatanaka.ecommerce.payment.event.PaymentCreatedEvent;
 import com.hatanaka.ecommerce.payment.streaming.CheckoutProcessor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
+@RequiredArgsConstructor
 public class CheckoutCreatedListener {
 
-    @StreamListener(CheckoutProcessor.INPUT)
-    public void handler(){
+    private final CheckoutProcessor checkoutProcessor;
 
+    @StreamListener(CheckoutProcessor.INPUT)
+    public void handler(Message<?> message){
+
+        final PaymentCreatedEvent paymentCreatedEvent = PaymentCreatedEvent.newBuilder()
+//              .setCheckoutCode(event.getCheckoutCode())
+//              .setCheckoutStatus(event.getStatus())
+                .setPaymentCode(UUID.randomUUID().toString())
+                .build();
+        checkoutProcessor.output().send(MessageBuilder.withPayload(paymentCreatedEvent).build());
     }
 }
