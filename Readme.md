b1cb6f97580ef9069f1814a3a788af942f2b7191
schott-anderson <schott.anderson@hotmail.com>
2021-09-23 12:44:01 -0300

    Atualiza API para retornar na API de checkout status dos pagamentos e adiciona arquivo readme

---
+++ Readme.md
@@ -0,0 +1,9 @@
+<h2>Ecommerce API Payment</h2>
+
+<p>Neste projeto prático iremos desenvolver uma solução de e-commerce com a arquitetura de microsserviços e aplicar a integração entre eles orientada a eventos com Apache Kafka e garantir a compatibilidade entre da comunicação dos microsserviços com Schema Registry.
+Para isso, programaremos em Java utilizando a stack do Spring (Spring Boot, Spring Cloud Streams).
+Dividimos este projeto em duas etapas, APY de pagamentos (payment), e API para registro de compras (checkout)
+</p>
+
+<p>Desenvolvido pela <a href="https://digitalinnovation.one/">DIO</a> </br>
+Orientação: <a href="https://github.com/hatanakadaniel">Daniel Hatanaka</a> </p>
--- src/main/java/com/hatanaka/ecommerce/payment/listener/CheckoutCreatedListener.java
+++ src/main/java/com/hatanaka/ecommerce/payment/listener/CheckoutCreatedListener.java
@@ -1,8 +1,12 @@
 package com.hatanaka.ecommerce.payment.listener;
 
+import com.hatanaka.ecommerce.checkout.event.CheckoutCreatedEvent;
+import com.hatanaka.ecommerce.payment.entity.PaymentEntity;
 import com.hatanaka.ecommerce.payment.event.PaymentCreatedEvent;
+import com.hatanaka.ecommerce.payment.service.PaymentService;
 import com.hatanaka.ecommerce.payment.streaming.CheckoutProcessor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
@@ -10,19 +14,24 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CheckoutCreatedListener {
 
     private final CheckoutProcessor checkoutProcessor;
 
-    @StreamListener(CheckoutProcessor.INPUT)
-    public void handler(Message<?> message){
+    private final PaymentService paymentService;
 
+    @StreamListener(CheckoutProcessor.INPUT)
+    public void handler(CheckoutCreatedEvent checkoutCreatedEvent){
+        log.info("checkoutCreatedEvent={}", checkoutCreatedEvent);
+        final PaymentEntity paymentEntity = paymentService.create(checkoutCreatedEvent).orElseThrow();
         final PaymentCreatedEvent paymentCreatedEvent = PaymentCreatedEvent.newBuilder()
-//              .setCheckoutCode(event.getCheckoutCode())
-//              .setCheckoutStatus(event.getStatus())
-                .setPaymentCode(UUID.randomUUID().toString())
+                .setCheckoutCode(paymentEntity.getCheckoutCode())
+                .setPaymentCode(paymentEntity.getCode())
                 .build();
         checkoutProcessor.output().send(MessageBuilder.withPayload(paymentCreatedEvent).build());
     }
